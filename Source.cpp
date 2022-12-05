cf38dd4921f762d86bfa280570795c99bec58dc4
Juancholoz <juandiegolozada123@gmail.com>
2022-12-05 18:26:21 -0500

    Dante for

--- Source.cpp
+++ Source.cpp
@@ -8,4 +8,7 @@ int main()
     cout << "Hello World!\n";
     cin >> i;
     cout << "HOLA ESTA ES UNA PRUEBA DE ESTE CODIGO";
+    for (i = 0; i < 3; i++) {
+        cout << "dante " << i + 1;
+    }
 }
